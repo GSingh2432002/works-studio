a12acdb54bc2aefd0947c15e004373a7a7ab4671
Gaurav Kumar Singh <gsingh332211@gmail.com>
2025-02-13 23:32:19 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+function loader() {
+  var tl = gsap.timeline();
+
+  tl.to("#yellow1", {
+    top: "-100%",
+    delay: 0.5,
+    duration: 0.7,
+    ease: "expo.out",
+  });
+
+  tl.from(
+    "#yellow2",
+    {
+      top: "100%",
+      delay: 0.6,
+      duration: 0.7,
+      ease: "expo.out",
+    },
+    "same"
+  );
+
+  tl.to(
+    "#loader h1",
+    {
+      delay: 0.6,
+      duration: 0.7,
+      color: "black",
+    },
+    "same"
+  );
+
+  tl.to("#loader", {
+    opacity: 0,
+  });
+
+  tl.to("#loader", {
+    display: "none",
+  });
+}
+loader();
+
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true,
+});
